3293118991a6292ef2c79f6eed39993b62a9778b
Eunsong <eunsong.choi@gmail.com>
2014-10-28 22:04:59 -0400

    remove tabs

--- src/mymd/dihedral/Dihedral.java
+++ src/mymd/dihedral/Dihedral.java
@@ -4,16 +4,16 @@ import mymd.MdSystem;
 
 public interface Dihedral<T extends MdSystem<?>>{
 
-	public int geti();
-	public int getj();
-	public int getk();
-	public int getl();
+    public int geti();
+    public int getj();
+    public int getk();
+    public int getl();
 
-	public double getAngleRadians(T sys);
+    public double getAngleRadians(T sys);
 
-	public void updateForce(T sys);
+    public void updateForce(T sys);
 
-	public double getEnergy(T sys);
+    public double getEnergy(T sys);
 
 
 }
