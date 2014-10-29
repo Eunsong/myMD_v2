a5e19769a178bae7cbce004d4a84045e2dc8530c
Eunsong <eunsong.choi@gmail.com>
2014-10-28 21:57:09 -0400

    remove tabs

--- src/mymd/angle/Angle.java
+++ src/mymd/angle/Angle.java
@@ -1,25 +1,25 @@
 package mymd.angle;
-	
+    
 import mymd.MdSystem;
 
 public interface Angle<T extends MdSystem<?>>{
 
-	public int geti();
-	public int getj();
-	public int getk();
+    public int geti();
+    public int getj();
+    public int getk();
 
-	public double getk0();
-	
-	public double getTheta0();
+    public double getk0();
+    
+    public double getTheta0();
 
-	/**
-	 * getAngleRadians() method returns an angle i-j-k in radians
-	 */
-	public double getAngleRadians(T sys); 
+    /**
+     * getAngleRadians() method returns an angle i-j-k in radians
+     */
+    public double getAngleRadians(T sys); 
 
-	public void updateForce(T sys);
+    public void updateForce(T sys);
 
-	public double getEnergy(T sys); 
+    public double getEnergy(T sys); 
 
 
 }
